fc0ae345faac53eaf06048f4d3603c7d990e6d83
Frosted-Raven <frost.caisse@gmail.com>
2026-04-11 09:47:05 -0700

    saving progress before the rebase

--- src/space/object/object.hpp
+++ src/space/object/object.hpp
@@ -1,10 +1,12 @@
 #pragma once
 
 #include <lager/effect.hpp>
+#include <immer/flex_vector.hpp>
 
 #include <space/utility/spacial_nav.hpp>
 #include <space/object/object_descriptor.hpp>
 #include <space/medium.hpp>
+#include <space/gridd.hpp>
 
 #include <variant>
 
@@ -17,5 +19,6 @@ namespace space::object{
         medium::medium_model medium;
         utility::sdf sdf_model;
 
+        immer::flex_vector<grid::voxel> voxels;
     };
 }
